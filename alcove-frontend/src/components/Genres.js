020832f9b8f4c81bd678bec600f0859b4b54df20
proiacm <proiacm@gmail.com>
2020-08-12 12:03:01 -0600

    boostrap card component for genres

--- alcove-frontend/src/components/Genres.js
+++ alcove-frontend/src/components/Genres.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 const Genres = props => {
 
-    const genres = props.genres.map((genre, i) => <li key={i}>{genre.name}</li>)
-    // console.log(genre)
+    const genres = props.genres.map((genre, i) => 
+        <ul key={i}> <div className="card">
+        <div className="card-body">
+            <h5 className="card-title">{genre.name}</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="card-link">Add Book</a>
+        </div>
+        </div>
+        </ul>
+        )
+        
     return (
         <div>
             { props.loading ? <h4>Loading...</h4> : genres }
         </div>
+      
     )
 }
 
 export default Genres
+
