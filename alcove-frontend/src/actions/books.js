05870c47c63d97ca25758dbdea08ec043142b666
proiacm <proiacm@gmail.com>
2020-08-12 12:35:48 -0600

    build fetch books action

---
+++ alcove-frontend/src/actions/books.js
@@ -0,0 +1,9 @@
+export const getBooks = () => {
+    return dispatch => {
+        dispatch({ type: 'LOADING_BOOKS'})
+        fetch('/books')
+        .then(res => res.json())
+        .then(books => dispatch({ type: 'BOOKS_LOADED', payload: books}))
+    }
+}
+
--- alcove-frontend/src/components/Genres.js
+++ alcove-frontend/src/components/Genres.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Books from '../components/Books'
 
 const Genres = props => {
 
@@ -6,7 +7,7 @@ const Genres = props => {
         <div key={i}> <div className="card">
         <div className="card-body">
             <h5 className="card-title">{genre.name}</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            {/* <p className="card-text"><Books/></p> */}
             <a href="#" className="card-link">Add Book</a>
         </div> 
         </div><br/>
