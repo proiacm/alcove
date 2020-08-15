d136495c5d57600d5cda5eec9d7b6d331b605990
proiacm <proiacm@gmail.com>
2020-08-14 19:34:22 -0600

    clean up genre component

--- alcove-frontend/src/components/Genre.js
+++ alcove-frontend/src/components/Genre.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 const Genre = props => {
     
     return (
         <div className="card text-center" >
-        <div className="card-body">
-            <h3 className="card-title">{props.genre.name}</h3>
-            <Link to={`/genres/${props.genre.id}`}>View Books in this Genre</Link>
-           
-        </div>
+            <div className="card-body">
+                <h3 className="card-title">{props.genre.name}</h3>
+                <Link to={`/genres/${props.genre.id}`}>View Books in this Genre</Link>
+            </div>
         </div>
       
     )
 }
 
 export default Genre
-
